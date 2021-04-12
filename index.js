a7b45388ca8379c188d55685a976d36a0f3ea3b2
Sandra Geovany <sandra.geovany@gmail.com>
2021-04-12 14:37:18 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [];
+    this.length= this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => a - b);
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if(this.length ===0 || pos>this.length-1){
+      throw new Error('OutOfBounds');
+    }else
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    if(this.items.length===0){
+      throw new Error('EmptySortedList');
+    }else
+   return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if(this.items.length===0){
+      throw new Error('EmptySortedList');
+    }else
+   return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+   let sum =0;
+    if(this.items.length===0){
+      return 0;
+    }else{
+      for(let i=0; i<this.items.length; i++){
+        sum+= this.items[i];
+      }
+      return sum;
+    }
+   
+  }
+
+  avg() {
+    if(this.items.length===0){
+      throw new Error('EmptySortedList');
+    }else
+    return this.sum()/this.items.length
+
+  }
 }
 
 module.exports = SortedList;
